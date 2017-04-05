cafb86d30abc6587b798719e2812e7baec87ac12
Sabrina Niklaus <sabrinamarlo@Sabrinas-MBP.hsd1.wa.comcast.net>
2017-04-04 20:22:46 -0700

    Created initial project

---
+++ CalcArrayPoints/CalcArrayPoints/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CalcArrayPoints
+//
+//  Created by Sabrina Niklaus on 4/4/17.
+//  Copyright © 2017 Sabrina Niklaus. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
