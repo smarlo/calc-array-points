0cb81eb9be662194438ae79d5d13d6cb0db0bd2b
Sabrina Niklaus <sabrinamarlo@Sabrinas-MBP.hsd1.wa.comcast.net>
2017-04-05 14:53:16 -0700

    Ran unit tests, still issues with dictionary points

--- CalcArrayPoints/CalcArrayPoints/main.swift
+++ CalcArrayPoints/CalcArrayPoints/main.swift
@@ -9,34 +9,34 @@
 import Foundation
 
 // Generic math operation
-func mathOperation(left : Int, right : Int, operation : (Int, Int) -> Int) -> Int {
+func mathOperation(left: Int, right: Int, operation: (Int, Int) -> Int) -> Int {
     return operation(left, right)
 }
 
 // Basic calculator operations
-func add(left : Int, right : Int ) -> Int {
+func add(left: Int, right: Int ) -> Int {
     return left + right
 }
 
-func subtract(left : Int, right : Int ) -> Int {
+func subtract(left: Int, right: Int ) -> Int {
     return left - right
 }
 
-func multiply(left : Int, right : Int ) -> Int {
+func multiply(left: Int, right: Int ) -> Int {
     return left * right
 }
 
-func divide(left : Int, right : Int ) -> Int {
+func divide(left: Int, right : Int ) -> Int {
     return left / right
 }
 
 // Generic math operation with input array
-func reduce(array : [Int], operation : ([Int]) -> Int) -> Int {
+func reduce(array: [Int], operation: ([Int]) -> Int) -> Int {
     return operation(array)
 }
 
 // Calculator functions with input array
-func add(array : [Int]) -> Int {
+func add(array: [Int]) -> Int {
     var sum = 0
     for num in array {
         sum += num
@@ -44,7 +44,7 @@ func add(array : [Int]) -> Int {
     return sum
 }
 
-func subtract(array : [Int]) -> Int {
+func subtract(array: [Int]) -> Int {
     var diff = 0
     for num in array {
         diff -= num
@@ -52,7 +52,15 @@ func subtract(array : [Int]) -> Int {
     return diff
 }
 
-func average(array : [Int]) -> Int {
+func multiply(array: [Int]) -> Int {
+    var product = 0
+    for num in array {
+        product = num * product
+    }
+    return product 
+}
+
+func average(array: [Int]) -> Int {
     var sum = 0
     for num in array {
         sum += num
@@ -60,18 +68,18 @@ func average(array : [Int]) -> Int {
     return sum / array.count
 }
 
-func count(array : [Int]) -> Int {
+func count(array: [Int]) -> Int {
     return array.count
 }
 
 // Points
-func add(p1 : (Int, Int), p2 : (Int, Int)) -> (Int, Int) {
+func add(p1: (Int, Int), p2: (Int, Int)) -> (Int, Int) {
     let num1 = p1.0 + p2.0
     let num2 = p1.1 + p2.1
     return (num1, num2)
 }
 
-func subtract(p1 : (Int, Int), p2 : (Int, Int)) -> (Int, Int) {
+func subtract(p1: (Int, Int), p2: (Int, Int)) -> (Int, Int) {
     let num1 = p1.0 - p2.0
     let num2 = p1.1 - p2.1
     return (num1, num2)
@@ -101,39 +109,39 @@ func subtract(_ points: (Int, Int)...) -> (Int, Int) {
 // Dictionary representation 
 
 // Ints
-func add(p1: [String : Int], p2: [String : Int]) -> [String : Int]? {
+func add(p1: [String : Int]?, p2: [String : Int]?) -> [String : Int] {
     var sum = ["x": Int(), "y": Int()]
-    if (p1["x"] != nil && p2["x"] != nil && p1["y"] != nil && p2["y"] != nil) {
-        sum["x"] = p1["x"]! + p2["y"]!
-        sum["y"] = p1["y"]! + p2["y"]!
+    if (p1?["x"] != nil && p2?["x"] != nil && p1?["y"] != nil && p2?["y"] != nil) {
+        sum["x"] = (p1?["x"])! + (p2?["y"])!
+        sum["y"] = (p1?["y"])! + (p2?["y"])!
     }
     return sum
 }
 
-func subtract(p1: [String : Int], p2: [String : Int]) -> [String : Int]? {
+func subtract(p1: [String : Int]?, p2: [String : Int]?) -> [String : Int] {
     var diff = ["x": Int(), "y": Int()]
-    if (p1["x"] != nil && p2["x"] != nil && p1["y"] != nil && p2["y"] != nil) {
-        diff["x"] = p1["x"]! - p2["y"]!
-        diff["y"] = p1["y"]! - p2["y"]!
+    if (p1?["x"] != nil && p2?["x"] != nil && p1?["y"] != nil && p2?["y"] != nil) {
+        diff["x"] = (p1?["x"]!)! - (p2?["y"]!)!
+        diff["y"] = (p1?["y"]!)! - (p2?["y"]!)!
     }
     return diff
 }
 
 // Doubles
-func add(p1: [String : Double], p2: [String : Double]) -> [String : Double]? {
+func add(p1: [String : Double]?, p2: [String : Double]?) -> [String : Double] {
     var sum = ["x": Double(), "y": Double()]
-    if (p1["x"] != nil && p2["x"] != nil && p1["y"] != nil && p2["y"] != nil) {
-        sum["x"] = p1["x"]! + p2["y"]!
-        sum["y"] = p1["y"]! + p2["y"]!
+    if (p1?["x"] != nil && p2?["x"] != nil && p1?["y"] != nil && p2?["y"] != nil) {
+        sum["x"] = (p1?["x"]!)! + (p2?["y"]!)!
+        sum["y"] = (p1?["y"]!)! + (p2?["y"]!)!
     }
     return sum
 }
 
-func subtract(p1: [String : Double], p2: [String : Double]) -> [String : Double]? {
+func subtract(p1: [String : Double]?, p2: [String : Double]?) -> [String : Double] {
     var diff = ["x": Double(), "y": Double()]
-    if (p1["x"] != nil && p2["x"] != nil && p1["y"] != nil && p2["y"] != nil) {
-        diff["x"] = p1["x"]! - p2["y"]!
-        diff["y"] = p1["y"]! - p2["y"]!
+    if (p1?["x"] != nil && p2?["x"] != nil && p1?["y"] != nil && p2?["y"] != nil) {
+        diff["x"] = (p1?["x"]!)! - (p2?["y"]!)!
+        diff["y"] = (p1?["y"]!)! - (p2?["y"]!)!
     }
     return diff
 }
